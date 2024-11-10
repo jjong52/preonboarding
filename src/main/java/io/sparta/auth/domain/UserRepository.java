217d09f3a9f3b2a15aebd0750e47c98b9198b9dd
suin <suin505@gmail.com>
2024-11-10 14:31:31 +0900

    feat: 로그인 구현

--- src/main/java/io/sparta/auth/domain/UserRepository.java
+++ src/main/java/io/sparta/auth/domain/UserRepository.java
@@ -2,6 +2,10 @@ package io.sparta.auth.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
